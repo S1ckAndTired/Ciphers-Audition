cce2a962360cffaa22a4273015b34fbdedc36a56
SickAndTired <78124142+S1ckAndTired@users.noreply.github.com>
2023-04-25 19:02:48 -0300

    Update cipher-audition.sh

--- cipher-audition.sh
+++ cipher-audition.sh
@@ -1,15 +1,23 @@
 #!/bin/bash
 
 
-OnRed='\033[41m'
+Orange='\033[0;33m'
 Yellow='\033[1;33m'
 Red='\033[0;31m'
 Green='\033[0;32m'
 NC='\033[0m'
 
-#for cipher in $(nmap -Pn --script ssl-enum-ciphers -p 443 $1 | awk '{print $2}' | grep "TLS_")
-for cipher in $(cat b)
+
+data=$(nmap -Pn --script ssl-enum-ciphers -p 443 $1 | awk '{print $2}' | grep "TLS" | sed 's/://g')
+for ciphers in $data
 do
+  echo $ciphers >> .out
+done
+cat .out | sed -n '/^TLSv/{h;n}; /^TLS_/{G;s/\n/-/p}' >> .ready-to-go
+for ciphers in $(cat .ready-to-go)
+do
+  cipher=$(echo $ciphers | awk -F "-" '{print $1}')
+  version=$(echo $ciphers | awk -F "-" '{print $2}')
   response=$(curl -s https://ciphersuite.info/cs/$cipher/)
   result1=$(echo $response | sed 's/>/\n/g' | sed 's/</\n/g' | grep "^$cipher" | sed 's/ *$//')
   result2=$(echo $response | sed 's/> /\n/g' | grep "span class=\"text-" | awk -F "[><]" '{print $3}')  
@@ -24,5 +32,6 @@ do
   else
     echo -e "[$Green$result2$NC] - [$Green$result1$NC] - [$version]"
   fi
-  #sleep 2
-  done
+  sleep 2
+done
+rm .out .ready-to-go
